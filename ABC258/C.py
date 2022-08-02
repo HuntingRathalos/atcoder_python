3f16a1cfaee72cfc708c27464c6ffb98135e50ff
HuntingRathalos <huntingandrathalos@gmail.com>
2022-08-02 12:54:06 +0900

    ABC258_C

--- ABC258/C.py
+++ ABC258/C.py
@@ -1,4 +1,15 @@
-# from sys import stdin
+from sys import stdin
 
-# N, Q = map(int, stdin.readline().split())
-# S = stdin.readline().rstrip()
+N, Q = map(int, stdin.readline().split())
+S = stdin.readline().rstrip()
+
+start = 0
+result = []
+
+for _ in range(Q):
+    t, x = map(int, stdin.readline().split())
+    if t == 1:
+        start = (start - x) % len(S)
+    elif t == 2:
+        result.append(S[(start + x - 1) % len(S)])
+print(*result, sep="\n")
