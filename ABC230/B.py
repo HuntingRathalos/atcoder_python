f9f79bb2c063d71da96006ac346bd4a0e6eaded1
HuntingRathalos <huntingandrathalos@gmail.com>
2022-08-09 23:39:34 +0900

    ABC230_B

--- ABC230/B.py
+++ ABC230/B.py
@@ -1,11 +1,3 @@
-from collections import Counter
-
-N = int(input())
-C = Counter()
-for _ in range(N):
-    s = input()
-    C[s] += 1
-mc = C.most_common(1)  # [("takahashi", 3)] のような長さ1のリストが得られます
-m_tuple = mc[0]  # ("takahashi", 3) を取り出します
-m_name, m_count = m_tuple
-print(m_name)
+S = input()
+T = "oxx" * 100  # 300文字にしてメモリの節約
+print("Yes" if S in T else "No")
