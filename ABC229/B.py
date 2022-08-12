63d9aa29c234525f8457d416f9685f6c3ed3af55
HuntingRathalos <huntingandrathalos@gmail.com>
2022-08-12 18:13:12 +0900

    ABC229_B

--- ABC229/B.py
+++ ABC229/B.py
@@ -1,3 +1,10 @@
-S = input()
-T = "oxx" * 100  # 300文字にしてメモリの節約
-print("Yes" if S in T else "No")
+def solve():
+    A, B = input().split()
+    X, Y = A.zfill(19), B.zfill(19)  # 0000...229のような、左から0埋めされた長さ20の文字列ができます
+    for x, y in zip(X, Y):
+        if int(x) + int(y) >= 10:
+            return "Hard"
+    return "Easy"
+
+
+print(solve())
