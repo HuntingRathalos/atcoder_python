f4a9a0bb30f2b6bd0278a8174d3851a654b7f626
HuntingRathalos <huntingandrathalos@gmail.com>
2022-08-12 22:52:38 +0900

    ABC227_B

--- ABC227/B.py
+++ ABC227/B.py
@@ -1,9 +1,18 @@
-N, X = map(int, input().split())
-A = [0] + list(map(int, input().split()))  # A[1]が友達1の情報を表すようにしたいので、A[0]にダミーを入れてずらします
-flag = [False] * (N + 1)  # flag[i] が True なら、友達iは秘密を知っています
-flag[X] = True
-now = X  # はじめに秘密を知ったのは友達Xです
-while not flag[A[now]]:  # 友達A_iがまだ秘密を知らなければ、A_iに伝えます（知っていればそこで終了です）
-    now = A[now]
-    flag[now] = True
-print(flag.count(True))
+def calc(a, b):
+    return 4 * a * b + 3 * a + 3 * b
+
+N = int(input())
+S = list(map(int, input().split()))
+T = set()
+# f(a,b)=4ab+3a+3b（最大値1000）より、aに1を代入するとbの最大は142となる。（bも同様）
+for a in range(1, 143):
+    for b in range(1, 143):
+        T.add(calc(a, b))
+
+ans = 0
+
+for x in S:
+    if x not in T:
+        ans += 1
+
+print(ans)
