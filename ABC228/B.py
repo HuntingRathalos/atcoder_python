38eb3fb6affceb078bb856f7b0798e41a36039a8
HuntingRathalos <huntingandrathalos@gmail.com>
2022-08-12 19:04:56 +0900

    ABC228_B

--- ABC228/B.py
+++ ABC228/B.py
@@ -1,10 +1,9 @@
-def solve():
-    A, B = input().split()
-    X, Y = A.zfill(19), B.zfill(19)  # 0000...229のような、左から0埋めされた長さ20の文字列ができます
-    for x, y in zip(X, Y):
-        if int(x) + int(y) >= 10:
-            return "Hard"
-    return "Easy"
-
-
-print(solve())
+N, X = map(int, input().split())
+A = [0] + list(map(int, input().split()))  # A[1]が友達1の情報を表すようにしたいので、A[0]にダミーを入れてずらします
+flag = [False] * (N + 1)  # flag[i] が True なら、友達iは秘密を知っています
+flag[X] = True
+now = X  # はじめに秘密を知ったのは友達Xです
+while not flag[A[now]]:  # 友達A_iがまだ秘密を知らなければ、A_iに伝えます（知っていればそこで終了です）
+    now = A[now]
+    flag[now] = True
+print(flag.count(True))
