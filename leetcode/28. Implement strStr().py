80913268d78b52c5915c5e1507f24b18fe404a03
HuntingRathalos <huntingandrathalos@gmail.com>
2022-08-17 22:57:25 +0900

    leetcode_28

---
+++ leetcode/28. Implement strStr().py
@@ -0,0 +1,9 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        nl, ml = len(needle), len(haystack)
+        if ml < nl:
+            return -1
+        for i in range(ml - nl + 1):
+            if haystack[i:i+nl] == needle:
+                return i
+        return -1
