f9aca75ff901f8a3e45a59e072dd98031cda71f8
HuntingRathalos <huntingandrathalos@gmail.com>
2022-09-07 20:26:02 +0900

    ABC220_A 2

--- ABC220/A.py
+++ ABC220/A.py
@@ -1,2 +1,10 @@
-A, B = map(int, input().split())
-print(32 ** (A-B))
+A, B, C = map(int, input().split())
+# 『B 以下で最大の C の倍数』は ⌊BC⌋×C⌊BC⌋×C で求められます
+# （B を C で割って切り捨てて CC を掛ける）
+x = B // C * C
+print(x if x >= A else -1)
+
+# for i in range(A, B + 1):
+#     if i % C == 0:
+#         print(i)
+# print(-1)
