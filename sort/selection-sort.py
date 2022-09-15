9ec3cd0828d74266434eb2c1416e5cf5f527ea74
HuntingRathalos <huntingandrathalos@gmail.com>
2022-09-16 06:11:47 +0900

    選択ソート練習

---
+++ sort/selection-sort.py
@@ -0,0 +1,13 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+for k in range(N-1):
+    # 未ソートの要素のうち最小値がある位置を求める
+    min_element_index = A[k:].index(min(A[k:])) + k
+
+    # 未ソートの要素の先頭 A[k] と最小値を入れ替え、
+    # 次のループでは A[0]...A[k] をソート済みとみなす
+    A[k], A[min_element_index] = A[min_element_index], A[k]
+
+    # 現時点での配列の中身を出力する
+    print(*A)
